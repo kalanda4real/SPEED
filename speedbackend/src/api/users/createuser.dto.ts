b48ec1a1b2730881bfdbfb4318a19b2e8ab4fcb4
kalanda4real <142495537+kalanda4real@users.noreply.github.com>
2024-09-29 22:43:41 +1300

    updated credentials that the users will input in the database

--- speedbackend/src/api/users/createuser.dto.ts
+++ speedbackend/src/api/users/createuser.dto.ts
@@ -1,7 +1,6 @@
 import {Date } from 'mongoose'
 
 export class CreateuserDto{
-    name:string;
-    email: string;
-    birthday: Date;
+    username:string;
+    password: Date;
 }
--- speedbackend/src/api/users/users.schema.ts
+++ speedbackend/src/api/users/users.schema.ts
@@ -5,14 +5,11 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  @Prop({ required: true, unique: true})
+  username: string;
+
   @Prop({ required: true })
-  name: string;
-
-  @Prop({ required: true, unique: true })
-  email: string;
-
-  @Prop({ type: Date, required: true })
-  birthday: Date;
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
