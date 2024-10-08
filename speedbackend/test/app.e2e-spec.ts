a0743cfff65452065ddd19c871b89ca228fe8552
azelsaralaeva <saralaeva.azel@gmail.com>
2024-10-08 14:05:44 +1300

    Creating unit tests

--- speedbackend/test/app.e2e-spec.ts
+++ speedbackend/test/app.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateArticleDto } from 'src/api/articles/createarticle.dto';
+import { ArticleController } from 'src/api/articles/article.controller';
+import { ArticleService } from 'src/api/articles/article.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +23,43 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+});
+
+describe('ArticleController (e2e)', () => {
+  let articleController: ArticleController;
+  let articleService: ArticleService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [ArticleController],
+      providers: [
+        {
+          provide: ArticleService,
+          useValue: {
+            create: jest.fn(), // Mock the create method
+          },
+        },
+      ],
+    }).compile();
+
+    articleController = moduleFixture.get<ArticleController>(ArticleController);
+    articleService = moduleFixture.get<ArticleService>(ArticleService);
+  });
+
+  it('should create an article (POST)', async () => {
+    const createArticleDto: CreateArticleDto = {
+      author: 'John Doe',
+      title: 'Best Practices in Software Engineering',
+      journal: 'SE Journal',
+      year: '2024',
+      submitter_name: 'Jane Smith',
+      submitter_email: 'jane.smith@example.com',
+      moderation_status: 'pending',
+      submitted_date: '2024-10-07', //FIX THIS ??
+    };
+
+    await articleController.addArticle(createArticleDto);
+    expect(articleService.create).toHaveBeenCalledWith(createArticleDto);
   });
 });
